b4008ba6093593246443182ba49a2931aa87d672
dionis <dionis.futura@gmail.com>
2022-06-08 10:28:36 -0300

    first commit

---
+++ Domain/Commands/Contracts/ICommand.cs
@@ -0,0 +1,9 @@
+﻿using Flunt.Validations;
+
+namespace Domain.Commands.Contracts
+{
+    public interface ICommand : IValidatable
+    {
+
+    }
+}
---
+++ Domain/Entities/Task.cs
@@ -0,0 +1,28 @@
+﻿namespace Domain.Entities
+{
+    public class Task : Entity
+    {
+        public Task(string title, string user, DateTime date)
+        {
+            Title = title;
+            Done = false;
+            User = user;
+            Date = date;
+        }
+
+        public string Title { get; private set; }
+        public bool Done { get; private set; }
+        public DateTime Date { get; private set; }
+        public string User { get; private set; }
+
+        public void SetDone(bool done)
+        {
+            Done = done;
+        }
+
+        public void SetTitle(string title)
+        {
+            Title = title;
+        }
+    }
+}
